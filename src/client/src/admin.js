bd0a8157ea4d2fab2ea8114e898fbbdd9ca908de
Shawn Wildermuth <shawn@wildermuth.com>
2023-01-11 14:30:12 -0500

    Update to use Vuetify

--- src/client/src/admin.js
+++ src/client/src/admin.js
@@ -3,7 +3,7 @@ import App from './App.vue';
 import router from './router';
 
 // Vuetify
-import 'vuetify/styles'
+import 'vuetify/styles';
 import { createVuetify } from 'vuetify'
 import * as components from 'vuetify/components'
 import * as directives from 'vuetify/directives'
